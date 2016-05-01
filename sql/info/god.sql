cf2c8d91021d0eb71ed7b3ff2b0831844ede4df8
jabreaux <jabreaux@localhost.localdomain>
2016-05-01 09:07:20 -0400

    Shifted ids from 0, 0 will be random

--- sql/info/god.sql
+++ sql/info/god.sql
@@ -1,41 +1,42 @@
 INSERT INTO god (id, name,element) VALUES 
-	(0,'Void','Void'),
-	(1,'Ama''at','Order'),
-	(2,'Sether','Chaos'),
-	(3,'Seker','Light'),
-	(4,'Aset','Dark'),
-	(5,'Selket','Life'),
-	(6,'Asar','Death'),
-	(10,'Kur','Earth'),
-	(11,'Ilnet', 'Nature'),
-	(7,'Silvanus','Water'),
-	(8,'Anki','Wind'),
-	(9,'Rishum','Fire')
+	 (0,'Random','Random')
+	,(1,'Void','Void')
+	,(2,'Ama''at','Order')
+	,(3,'Sether','Chaos')
+	,(4,'Seker','Light')
+	,(5,'Aset','Dark')
+	,(6,'Selket','Life')
+	,(7,'Asar','Death')
+	,(8,'Silvanus','Water')
+	,(9,'Anki','Wind')
+	,(10,'Rishum','Fire')
+	,(11,'Kur','Earth')
+	,(12,'Ilnet', 'Nature')
 	;
 
 INSERT INTO godaffinity VALUES
 /* Spiritual Oppositions */
-	(1,2,2),
-	(2,1,2),
-	(3,4,2),
-	(4,3,2),
-	(5,6,2),
-	(6,5,2),
+	(2,3,2),
+	(3,2,2),
+	(4,5,2),
+	(5,4,2),
+	(6,7,2),
+	(7,6,2),
 /* Elemental Oppositions */
-	(7,9,2),
-	(9,7,0.5),
-	(7,10,0.5),
-	(10,7,2),
-	(10,8,0.5),
 	(8,10,2),
+	(10,8,0.5),
 	(8,11,0.5),
 	(11,8,2),
 	(11,9,0.5),
 	(9,11,2),
+	(9,12,0.5),
+	(12,9,2),
+	(12,10,0.5),
+	(10,12,2),
 /* Elemental Benefits */
-	(7,8,0),
 	(8,9,0),
 	(9,10,0),
 	(10,11,0),
-	(11,7,0)
+	(11,12,0),
+	(12,8,0)
 ;
